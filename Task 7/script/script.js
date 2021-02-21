71ac5840a6e40d79f4ba880743b69d09c78b357a
NewAlexey <alexey.krupenia@gmail.com>
2021-02-22 00:10:59 +0300

    fix bugs, add new features

--- Task 7/script/script.js
+++ Task 7/script/script.js
@@ -1,3 +1,5 @@
+selector = false;
+
 let screen = document.getElementById("screen");
 screen.innerHTML = "0";
 
@@ -17,8 +19,25 @@ function checkZero() {
 }
 
 function isEqualInInfo() {
-    let infoValue = info.innerHTML;
-    if (infoValue.search(/=/) > 0) clearScreen();
+    let valueInfo = info.innerHTML;
+    if (valueInfo.search(/=/) > 0) clearScreen();
+    // if (valueInfo[valueInfo.length - 1] !== ' =') clearScreen();
+}
+
+function insertInJournal() {
+    let result = info.innerHTML;
+    let journal = document.getElementById('journal');
+    let journalInfo = document.getElementById('journal').innerHTML;
+    if (journal.innerHTML === '') journal.innerHTML = `${result} ${screen.innerHTML}`
+    else journal.innerHTML = `${journalInfo}\n${result} ${screen.innerHTML}`;
+}
+
+
+let buttonClearJournal = document.getElementById("trash");
+buttonClearJournal.addEventListener("click", clearJournal);
+function clearJournal() {
+    let journal = document.getElementById('journal');
+    journal.innerHTML = '';
 }
 
 //Buttons for operators
@@ -26,11 +45,12 @@ function isEqualInInfo() {
 let buttonDel = document.getElementById("buttonDel");
 buttonDel.addEventListener("click", clickButtonDel);
 function clickButtonDel() {
-    let screenNumber = document.getElementById("screen");
-    let num = screenNumber.innerHTML;
-    if (num.length === 1) return (screenNumber.innerHTML = "0");
-    num = num.slice(0, num.length - 1);
-    return (screenNumber.innerHTML = num);
+    let num = screen.innerHTML;
+    if (info.innerHTML.indexOf("=") < 0) {
+        if (num.length === 1) return (screen.innerHTML = "0");
+        num = num.slice(0, num.length - 1);
+        screen.innerHTML = num;
+    }
 }
 
 let buttonDot = document.getElementById("buttonDot");
@@ -47,20 +67,31 @@ function clickButtonRevers() {
     num === "0" ? true : (screen.innerHTML = num * -1);
 }
 
-let buttonCalculate = document.getElementById("buttonCalculate");
-buttonCalculate.addEventListener("click", clickButtonCalculate);
-function clickButtonCalculate() {
+function calculateValues() {
     let resultScreen = screen.innerHTML;
     let resultInfo = info.innerHTML;
     let arrValue = (resultInfo + " " + resultScreen).split(" ");
-    let operator = arrValue[1];
-    let calcValue = 0;
-    if (operator === "+") calcValue = +arrValue[0] + +arrValue[2];
-    else if (operator === "-") calcValue = +arrValue[0] - +arrValue[2];
-    else if (operator === "*") calcValue = +arrValue[0] * +arrValue[2];
-    else calcValue = +arrValue[0] / +arrValue[2];
-    info.innerHTML = arrValue.join(" ") + " =";
-    screen.innerHTML = calcValue;
+    let calcValue = +arrValue[0];
+    for (let i = 1; i < arrValue.length; i += 2) {
+        if (arrValue[i] === "+") calcValue += +arrValue[i + 1];
+        else if (arrValue[i] === "-") calcValue -= +arrValue[i + 1];
+        else if (arrValue[i] === "*") calcValue *= +arrValue[i + 1];
+        else calcValue /= +arrValue[i + 1];
+    }
+    selector = true;
+    return [calcValue, arrValue];
+}
+
+let buttonCalculate = document.getElementById("buttonCalculate");
+buttonCalculate.addEventListener("click", clickButtonCalculate);
+function clickButtonCalculate() {
+    let arrCalcValue = calculateValues();
+    selector = false;
+    if (info.innerHTML.indexOf("=") < 0) {
+        info.innerHTML = arrCalcValue[1].join(" ") + " =";
+        screen.innerHTML = arrCalcValue[0];
+    }
+    insertInJournal();
 }
 
 let buttonPlus = document.getElementById("buttonPlus");
@@ -71,6 +102,13 @@ function clickButtonPlus() {
     if (valueInfo === "") {
         screen.innerHTML = "0";
         info.innerHTML = valueScreen + " +";
+    } else if (valueInfo.search(/=/) > 0) {
+        info.innerHTML = valueScreen + " +";
+        screen.innerHTML = "0";
+    } else if (valueScreen !== "0") {
+        let arrCalcValue = calculateValues();
+        info.innerHTML = arrCalcValue[1].join(" ") + " +";
+        screen.innerHTML = arrCalcValue[0];
     } else {
         info.innerHTML = valueInfo.slice(0, valueInfo.length - 1) + "+";
     }
@@ -84,6 +122,13 @@ function clickButtonMinus() {
     if (valueInfo === "") {
         screen.innerHTML = "0";
         info.innerHTML = valueScreen + " -";
+    } else if (valueInfo.search(/=/) > 0) {
+        info.innerHTML = valueScreen + " -";
+        screen.innerHTML = "0";
+    } else if (valueScreen !== "0") {
+        let arrCalcValue = calculateValues();
+        info.innerHTML = arrCalcValue[1].join(" ") + " -";
+        screen.innerHTML = arrCalcValue[0];
     } else {
         info.innerHTML = valueInfo.slice(0, valueInfo.length - 1) + "-";
     }
@@ -97,6 +142,13 @@ function clickButtonMult() {
     if (valueInfo === "") {
         screen.innerHTML = "0";
         info.innerHTML = valueScreen + " *";
+    } else if (valueInfo.search(/=/) > 0) {
+        info.innerHTML = valueScreen + " *";
+        screen.innerHTML = "0";
+    } else if (valueScreen !== "0") {
+        let arrCalcValue = calculateValues();
+        info.innerHTML = arrCalcValue[1].join(" ") + " *";
+        screen.innerHTML = arrCalcValue[0];
     } else {
         info.innerHTML = valueInfo.slice(0, valueInfo.length - 1) + "*";
     }
@@ -110,6 +162,13 @@ function clickButtonDivide() {
     if (valueInfo === "") {
         screen.innerHTML = "0";
         info.innerHTML = valueScreen + " /";
+    } else if (valueInfo.search(/=/) > 0) {
+        info.innerHTML = valueScreen + " /";
+        screen.innerHTML = "0";
+    } else if (valueScreen !== "0") {
+        let arrCalcValue = calculateValues();
+        info.innerHTML = arrCalcValue[1].join(" ") + " /";
+        screen.innerHTML = arrCalcValue[0];
     } else {
         info.innerHTML = valueInfo.slice(0, valueInfo.length - 1) + "/";
     }
@@ -120,89 +179,159 @@ function clickButtonDivide() {
 let buttonOne = document.getElementById("buttonOne");
 buttonOne.addEventListener("click", clickButtonOne);
 function clickButtonOne() {
-        isEqualInInfo()
-        checkZero();
-        let num = screen.innerHTML;
+    isEqualInInfo();
+    checkZero();
+    let num = screen.innerHTML;
+    if (screen.innerHTML === "") {
         screen.innerHTML = num + "1";
+    } else if (info.innerHTML.length > 0 && selector === true) {
+        selector = false;
+        screen.innerHTML = "1";
+    } else if (info.innerHTML.length > 0) {
+        screen.innerHTML = num + "1";
+    } else screen.innerHTML = num + "1";
 }
 
 let buttonTwo = document.getElementById("buttonTwo");
 buttonTwo.addEventListener("click", clickButtonTwo);
 function clickButtonTwo() {
-    isEqualInInfo()
+    isEqualInInfo();
     checkZero();
     let num = screen.innerHTML;
-    screen.innerHTML = num + "2";
+    if (screen.innerHTML === "") {
+        screen.innerHTML = num + "2";
+    } else if (info.innerHTML.length > 0 && selector === true) {
+        selector = false;
+        screen.innerHTML = "2";
+    } else if (info.innerHTML.length > 0) {
+        screen.innerHTML = num + "2";
+    } else screen.innerHTML = num + "2";
 }
 
 let buttonThree = document.getElementById("buttonThree");
 buttonThree.addEventListener("click", clickButtonThree);
 function clickButtonThree() {
-    isEqualInInfo()
+    isEqualInInfo();
     checkZero();
     let num = screen.innerHTML;
-    screen.innerHTML = num + "3";
+    if (screen.innerHTML === "") {
+        screen.innerHTML = num + "3";
+    } else if (info.innerHTML.length > 0 && selector === true) {
+        selector = false;
+        screen.innerHTML = "3";
+    } else if (info.innerHTML.length > 0) {
+        screen.innerHTML = num + "3";
+    } else screen.innerHTML = num + "3";
 }
 
 let buttonFour = document.getElementById("buttonFour");
 buttonFour.addEventListener("click", clickButtonFour);
 function clickButtonFour() {
-    isEqualInInfo()
+    isEqualInInfo();
     checkZero();
     let num = screen.innerHTML;
-    screen.innerHTML = num + "4";
+    if (screen.innerHTML === "") {
+        screen.innerHTML = num + "4";
+    } else if (info.innerHTML.length > 0 && selector === true) {
+        selector = false;
+        screen.innerHTML = "4";
+    } else if (info.innerHTML.length > 0) {
+        screen.innerHTML = num + "4";
+    } else screen.innerHTML = num + "4";
 }
 
 let buttonFive = document.getElementById("buttonFive");
 buttonFive.addEventListener("click", clickButtonFive);
 function clickButtonFive() {
-    isEqualInInfo()
+    isEqualInInfo();
     checkZero();
     let num = screen.innerHTML;
-    screen.innerHTML = num + "5";
+    if (screen.innerHTML === "") {
+        screen.innerHTML = num + "5";
+    } else if (info.innerHTML.length > 0 && selector === true) {
+        selector = false;
+        screen.innerHTML = "5";
+    } else if (info.innerHTML.length > 0) {
+        screen.innerHTML = num + "5";
+    } else screen.innerHTML = num + "5";
 }
 
 let buttonSix = document.getElementById("buttonSix");
 buttonSix.addEventListener("click", clickButtonSix);
 function clickButtonSix() {
-    isEqualInInfo()
+    isEqualInInfo();
     checkZero();
     let num = screen.innerHTML;
-    screen.innerHTML = num + "6";
+    if (screen.innerHTML === "") {
+        screen.innerHTML = num + "6";
+    } else if (info.innerHTML.length > 0 && selector === true) {
+        selector = false;
+        screen.innerHTML = "6";
+    } else if (info.innerHTML.length > 0) {
+        screen.innerHTML = num + "6";
+    } else screen.innerHTML = num + "6";
 }
 
 let buttonSeven = document.getElementById("buttonSeven");
 buttonSeven.addEventListener("click", clickButtonSeven);
 function clickButtonSeven() {
-    isEqualInInfo()
+    isEqualInInfo();
     checkZero();
     let num = screen.innerHTML;
-    screen.innerHTML = num + "7";
+    if (screen.innerHTML === "") {
+        screen.innerHTML = num + "7";
+    } else if (info.innerHTML.length > 0 && selector === true) {
+        selector = false;
+        screen.innerHTML = "7";
+    } else if (info.innerHTML.length > 0) {
+        screen.innerHTML = num + "7";
+    } else screen.innerHTML = num + "7";
 }
 
 let buttonEight = document.getElementById("buttonEight");
 buttonEight.addEventListener("click", clickButtonEight);
 function clickButtonEight() {
-    isEqualInInfo()
+    isEqualInInfo();
     checkZero();
     let num = screen.innerHTML;
-    screen.innerHTML = num + "8";
+    if (screen.innerHTML === "") {
+        screen.innerHTML = num + "8";
+    } else if (info.innerHTML.length > 0 && selector === true) {
+        selector = false;
+        screen.innerHTML = "8";
+    } else if (info.innerHTML.length > 0) {
+        screen.innerHTML = num + "8";
+    } else screen.innerHTML = num + "8";
 }
 
 let buttonNine = document.getElementById("buttonNine");
 buttonNine.addEventListener("click", clickButtonNine);
 function clickButtonNine() {
-    isEqualInInfo()
+    isEqualInInfo();
     checkZero();
     let num = screen.innerHTML;
-    screen.innerHTML = num + "9";
+    if (screen.innerHTML === "") {
+        screen.innerHTML = num + "9";
+    } else if (info.innerHTML.length > 0 && selector === true) {
+        selector = false;
+        screen.innerHTML = "9";
+    } else if (info.innerHTML.length > 0) {
+        screen.innerHTML = num + "9";
+    } else screen.innerHTML = num + "9";
 }
 
 let buttonZero = document.getElementById("buttonZero");
 buttonZero.addEventListener("click", clickButtonZero);
 function clickButtonZero() {
-    isEqualInInfo()
+    isEqualInInfo();
     checkZero();
     let num = screen.innerHTML;
-    screen.innerHTML = num + "0";
+    if (screen.innerHTML === "") {
+        screen.innerHTML = num + "0";
+    } else if (info.innerHTML.length > 0 && selector === true) {
+        selector = false;
+        screen.innerHTML = "0";
+    } else if (info.innerHTML.length > 0) {
+        screen.innerHTML = num + "0";
+    } else screen.innerHTML = num + "0";
 }
